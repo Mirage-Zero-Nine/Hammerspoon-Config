7141bef87bf015a33f96cf66de317f8d031fac83
Null <borismirage00@gmail.com>
2018-05-23 21:13:47 -0400

    Add functions

--- init.lua
+++ init.lua
@@ -1,5 +1,18 @@
 -- lua script for Hammerspoon
+
 require "InputSwitch"   
+require "MoveMouseBetweenScreen"
+require "MoveWindowBetweenScreen"
+require "ResizeWindow"
+require "SplitScreen"
 
+-- Start function written in Language.lua
 
+-- InputSwitch
 start_language_switch()
+
+-- MoveWindowBetweenScreen
+move_window()
+
+-- MoveMouseBetweenScreen
+move_mouse()
